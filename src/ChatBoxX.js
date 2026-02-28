32586fcddc1ea34a4736368491b928b5e4bb80dc
Henrique <henrique.al.marques@outlook.com>
2026-02-28 20:59:11 -0300

    Exercicios de decisao e repeticao

---
+++ src/ChatBoxX.js
@@ -0,0 +1,23 @@
+const comando = gets();
+
+switch (comando){
+    case "horario":
+    console.log("Atendemos das 8h as 18h")
+    break
+
+    case "telefone":
+    console.log("Nosso telefone e 4002-8922")
+    break
+
+    case "servicos":
+    console.log("Oferecemos suporte, vendas e consultoria")
+    break
+
+    case "servicos":
+    console.log("Oferecemos suporte, vendas e consultoria")
+    break
+
+    default:
+    console.log("Comando invalido")
+
+}
---
+++ src/chatbots.js
@@ -0,0 +1,14 @@
+const tipoMensagem = gets();
+let contador = 0;
+
+let mensagem = gets();
+
+while (mensagem !== "FIM") {
+
+    if (mensagem.startsWith(tipoMensagem + ":")) {
+        contador++;
+    }
+    mensagem = gets();
+}
+
+print(contador);
